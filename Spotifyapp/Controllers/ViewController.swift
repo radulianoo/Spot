fdf9fec5fd7fae311f6ffd67f6834d71f8517c81
radulianoo <octav@raduliano.tech>
2023-04-03 13:42:50 +0300

    Initial setup

---
+++ Spotifyapp/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotifyapp
+//
+//  Created by Octav Radulian on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        title = "Home"
+    }
+
+
+}
+
